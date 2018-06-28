95c1a277873a6d5be6e7371a909a15a91a19acbf
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-28 01:09:11 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,32 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-#
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
-# Make sure each ruby method returns a string containing a valid SQL statement.
-
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT projects.title, SUM(pledges.amount) FROM projects LEFT OUTER JOIN pledges
+  WHERE pledges.project_id = projects.id GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users LEFT OUTER JOIN pledges
+  WHERE pledges.user_id = users.id GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, (SUM(pledges.amount) - projects.funding_goal) FROM projects LEFT OUTER JOIN pledges
+  WHERE pledges.project_id = projects.id GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, SUM(pledges.amount FROM users LEFT OUTER JOIN pledges
+  WHERE pledges.user_id = users.id GROUP BY users.name ORDER BY pledges.amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+#{}"SELECT projects.category, pledges.amount FROM projects LEFT OUTER JOIN pledges
+#  WHERE projects.category = 'music' GROUP BY projects.category;"
+#  "SELECT projects.category, SUM(pledges.amount) FROM pledges"
+"SELECT projects.category, pledges.amount FROM projects LEFT OUTER JOIN pledges
+  GROUP BY projects.category HAVING projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, SUM(pledges.amount) FROM projects LEFT OUTER JOIN pledges
+  WHERE projects.id = pledges.project_id GROUP BY pproject.category;"
 end
